6f8ca168a64c710875ad472f592de3e6223cb482
f90 <daniel@dstoller.net>
2018-09-30 20:17:11 +0100

    URL fix

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,15 +2,15 @@ var store = [{
         "title": "Snowfall: A Very Special Video Game Controller",
         "excerpt":"Here is a short Youtube video explaining what this post it about. How did I do it? I will try to go through the main steps in the following. Understanding the wiring and connecting the Arduino To use the mat as a game controller, I had to first understand its...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Snowfall-a-very-special-video-game-controller/",
+        "url": "https://f90.github.io/Snowfall-a-very-special-video-game-controller/",
         "teaser":null},{
         "title": "Tensorflow Lstm For Language Modelling",
         "excerpt":"In this post, I will show you how to build an LSTM network for the task of character-based language modelling (predict the next character based on the previous ones), and apply it to generate lyrics. In general, the model allows you to generate new text as well as auto-complete your...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Tensorflow-LSTM-for-Language-Modelling/",
+        "url": "https://f90.github.io/Tensorflow-LSTM-for-Language-Modelling/",
         "teaser":null},{
         "title": "Spectrogram Input Normalisation For Neural Networks",
         "excerpt":"In this post, I want to talk about magnitude spectrograms as inputs and outputs of neural networks, and how to normalise them to help the training process. Introduction: Time-frequency representations, magnitude spectrogram When using neural networks for audio tasks, it is often advantageous to input not the audio waveform directly...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Spectrogram-input-normalisation-for-neural-networks/",
+        "url": "https://f90.github.io/Spectrogram-input-normalisation-for-neural-networks/",
         "teaser":null}]
