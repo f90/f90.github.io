73463ad2743db67c77316e88a3b038a191252ff8
f90 <daniel@dstoller.net>
2018-10-18 16:14:28 +0100

    Change base URL from f90.github.io to dans.world

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,20 +2,20 @@ var store = [{
         "title": "Snowfall: A Very Special Video Game Controller",
         "excerpt":"Here is a short Youtube video explaining what this post it about. How did I do it? I will try to go through the main steps in the following. Understanding the wiring and connecting the Arduino To use the mat as a game controller, I had to first understand its...","categories": [],
         "tags": [],
-        "url": "https://f90.github.io/Snowfall-a-very-special-video-game-controller/",
+        "url": "https://dans.world/Snowfall-a-very-special-video-game-controller/",
         "teaser":null},{
         "title": "Tensorflow LSTM for Language Modelling",
         "excerpt":"In this post, I will show you how to build an LSTM network for the task of character-based language modelling (predict the next character based on the previous ones), and apply it to generate lyrics. In general, the model allows you to generate new text as well as auto-complete your...","categories": [],
         "tags": [],
-        "url": "https://f90.github.io/Tensorflow-LSTM-for-Language-Modelling/",
+        "url": "https://dans.world/Tensorflow-LSTM-for-Language-Modelling/",
         "teaser":null},{
         "title": "Spectrogram Input Normalisation For Neural Networks",
         "excerpt":"In this post, I want to talk about magnitude spectrograms as inputs and outputs of neural networks, and how to normalise them to help the training process. Introduction: Time-frequency representations, magnitude spectrogram When using neural networks for audio tasks, it is often advantageous to input not the audio waveform directly...","categories": [],
         "tags": [],
-        "url": "https://f90.github.io/Spectrogram-input-normalisation-for-neural-networks/",
+        "url": "https://dans.world/Spectrogram-input-normalisation-for-neural-networks/",
         "teaser":null},{
         "title": "ISMIR - Paper overviews",
         "excerpt":"This year’s ISMIR was great as ever, this time featuring lots of deep learning - I suspect since it became much more easy to use with recently developed libraries lots of new, and surprisingly large, datasets (suited for the new deep learning era) and a fantastic boat tour through Paris!For...","categories": [],
         "tags": [],
-        "url": "https://f90.github.io/ISMIR-Summary/",
+        "url": "https://dans.world/ISMIR-Summary/",
         "teaser":null}]
