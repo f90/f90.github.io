bc3dfa59e307450c80f084567d3febe4a9f5355b
f90 <daniel@dstoller.net>
2019-06-26 20:59:51 +0100

    Updated bibliography

---
+++ _pages/publications.md
@@ -0,0 +1,12 @@
+﻿---
+title: List of publications
+permalink: /publications/
+---
+ 
+## Journal articles 
+
+{% bibliography --query @article[status!=editorial && status!=other && author^=Stoller] %}
+
+## Conference papers
+
+{% bibliography --query @inproceedings[status!=editorial && status!=other && author^=Stoller] %}
--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,30 +2,30 @@ var store = [{
         "title": "Snowfall: A Very Special Video Game Controller",
         "excerpt":"Here is a short Youtube video explaining what this post it about. How did I do it? I will try to go through the main steps in the following. Understanding the wiring and connecting the Arduino To use the mat as a game controller, I had to first understand its...","categories": [],
         "tags": [],
-        "url": "https://dans.world/Snowfall-a-very-special-video-game-controller/",
+        "url": "http://localhost:4000/Snowfall-a-very-special-video-game-controller/",
         "teaser":null},{
         "title": "Tensorflow LSTM for Language Modelling",
         "excerpt":"In this post, I will show you how to build an LSTM network for the task of character-based language modelling (predict the next character based on the previous ones), and apply it to generate lyrics. In general, the model allows you to generate new text as well as auto-complete your...","categories": [],
         "tags": [],
-        "url": "https://dans.world/Tensorflow-LSTM-for-Language-Modelling/",
+        "url": "http://localhost:4000/Tensorflow-LSTM-for-Language-Modelling/",
         "teaser":null},{
         "title": "Spectrogram Input Normalisation For Neural Networks",
         "excerpt":"In this post, I want to talk about magnitude spectrograms as inputs and outputs of neural networks, and how to normalise them to help the training process. Introduction: Time-frequency representations, magnitude spectrogram When using neural networks for audio tasks, it is often advantageous to input not the audio waveform directly...","categories": [],
         "tags": [],
-        "url": "https://dans.world/Spectrogram-input-normalisation-for-neural-networks/",
+        "url": "http://localhost:4000/Spectrogram-input-normalisation-for-neural-networks/",
         "teaser":null},{
         "title": "ISMIR 2018 - Paper Overviews",
         "excerpt":"This year’s ISMIR was great as ever, this time featuring lots of deep learning - I suspect since it became much more easy to use with recently developed libraries lots of new, and surprisingly large, datasets (suited for the new deep learning era) and a fantastic boat tour through Paris!For...","categories": [],
         "tags": [],
-        "url": "https://dans.world/ISMIR-Summary/",
+        "url": "http://localhost:4000/ISMIR-Summary/",
         "teaser":null},{
         "title": "Bounded output regression with neural networks",
         "excerpt":"Say we have a neural network (or some other model trainable with gradient descent) that performs supervised regression: For an input $x$, it outputs one or more real values $y$ as prediction, and tries to get as close to a given target value $\\hat{y}$ as possible. We also know that...","categories": [],
         "tags": [],
-        "url": "https://dans.world/Bounded-output-networks/",
+        "url": "http://localhost:4000/Bounded-output-networks/",
         "teaser":null},{
         "title": "Spotify Research Internship Report",
         "excerpt":"From June to September 2019, I took a break from my ongoing PhD and worked as a Research Intern at Spotify in London.I was under the supervision of Simon Durand and Tristan Jehan as part of the music intelligence (MIQ) team.Their work is also similar to my PhD, focused on...","categories": [],
         "tags": [],
-        "url": "https://dans.world/Spotify-internship/",
+        "url": "http://localhost:4000/Spotify-internship/",
         "teaser":null}]
